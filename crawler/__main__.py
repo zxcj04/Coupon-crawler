86ec45f3b5b75fa5490b321d4300f92b1b08b42c
FanRende <zxcj04@gmail.com>
2024-09-24 05:32:12 +0000

    feat: crawl every 5 sec
    This commit adds the `mortalprogressbar` package to the Pipfile. This package is required for the progress bar functionality in the code.
    
    Ref: #123

--- crawler/__main__.py
+++ crawler/__main__.py
@@ -1,9 +1,13 @@
+import time
+
+from progress import ProgressBar
+
 from . import CostcoDBSingleton
 from .lib import CostcoProduct, get_data
 
 
 def main():
-    costco_data = get_data()
+    costco_data = get_data(web=True)
     costco_db = CostcoDBSingleton.get_instance()
 
     products = costco_data.get("products")
@@ -33,4 +37,11 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    cnt = 0
+    # repeat the process every 5 seconds
+    with ProgressBar(total=10, prefix=f"Crawling data: {cnt}") as pb:
+        while cnt < 10:
+            main()
+            cnt += 1
+            pb.update(add=1, prefix=f"Crawling data: {cnt}")
+            time.sleep(5)
